2f6ca2e8c6a5195cb539f61bc86c9895991d6a2a
Lance <Lance@thelpstory.com>
2015-12-11 07:29:04 -0700

    cleanup

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+    init.then(makeRequest)
+
 });
+
+var reddit = 'https://www.reddit.com/r/Web_Development/';
+var hackerNews = 'https://news.ycombinator.com/';
+var mdn = 'https://developer.mozilla.org/en-US/docs/Web/JavaScript';
+var python = 'https://www.python.org/';
+
+var pythonFirst = $('#success-story-2 > blockquote > a').text();
+var redditFirst = $('.title > a').first().text()
+var hackerFirst = $('.title > a:nth-child(2)').first().text();
+var mdnFirst = $('#Join_the_JavaScript_community').text();
+
+var init = new Promise(function (resolve, reject) {
+    if (makeRequest({
+            source: reddit,
+            target: redditFirst
+        }).status === 200) {
+        resolve(true)
+    }
+    reject(false)
+
+})
+
+function makeRequest(result) {
+    request(source, function (error, response, html) {
+        if (!error) {
+            console.log('here')
+            var $ = cheerio.load(html);
+            if (target.indexOf('javascript') !== -1) {
+                return {
+                    status: 200,
+                    source: mdn,
+                    target: mdnFirst
+                }
+            } else {
+                return {
+                    status: 200,
+                    source: mdn,
+                    target: mdnFirst
+                }
+            }
+        }
+    })
+}
+
+
 
 module.exports = router;
